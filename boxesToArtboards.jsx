3473be07835b48b91dadf5604846e1c3ae1727a3
JaceyRaze <rzkharris@gmail.com>
2017-06-10 18:07:35 -0400

    Stroked Rectangles to Artboards
    https://www.reddit.com/r/AdobeIllustrator/comments/65x8l1/looking_for_a_way_to_add_multiple_art_boards_to_a/
    
    Created in response to the above thread for user live2last.
    Editing values of the two variable underneath items will customize the script.
    
    REQUIRES:
    ► At least one open illustrator file
    ► CYMK Color space (changed by File->Document Color Mode->CMYK Color)
    ► A stroked pathItem with a color/width that matches the following variables
    
    var stWidth = 0.5  
    var fColor = [0, 100, 0, 0]  (ie. [0% Cyan, 100% Magenta, 0% Yellow, 0% Black])
    
    stWidth = Stroke Width in Points (inches/72 = points)
    fColor = Color Array where [cyan, magenta, yellow, black]
    
    RAE 061017

---
+++ boxesToArtboards.jsx
@@ -0,0 +1,45 @@
+﻿/**
+* @@@BUILDINFO@@@ boxesToArtboards.jsx !Version! Sat Jun 10 2017 17:31:47 GMT-0400
+*/
+/*
+|   ****************************************
+|   Convert Stroked Rectangles to Artboards
+|   ****************************************
+|
+|   CREATED BY: Jacey Rae Harris
+|   rzkharris@gmail.com
+|
+*/
+$.bp(app.documents < 1)
+
+var items = app.activeDocument.activeLayer.pathItems
+var stWidth = 0.5
+var fColor = [0, 100, 0, 0]
+
+function compareCMYK(cmykColor, compareArray) {
+    if(
+    cmykColor.cyan == compareArray[0] &&
+    cmykColor.magenta == compareArray[1] &&
+    cmykColor.yellow == compareArray[2] &&
+    cmykColor.black == compareArray[3]
+        ){
+            return true
+            }
+        else{
+            return false
+            }
+    }
+
+function findBoxes(strokeWid, strokeCol) {
+    $.writeln(items.length)
+    for( i = 0; i < items.length; i++){
+        if(compareCMYK (items[i].strokeColor, strokeCol) && items[i].strokeWidth == strokeWid) {
+            app.activeDocument.artboards.add(items[i].geometricBounds)
+            }
+        }
+    if(app.activeDocument.artboards.length > 1) {
+        app.activeDocument.artboards[0].remove()
+        }
+    }
+
+findBoxes (stWidth, fColor)
